09747362cb3836501a49e3cd2c9bb3f805452514
Miksu21 <mikaahonen21@gmail.com>
2023-03-21 20:02:39 +0200

    first commit

---
+++ Tehtävä 4/Tehtävä 4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Tehtävä_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 4 C# Perus: Palkan lasku
+            // koodi pyydä käyttäjältä 4 eri arvoa ja suorita lasku
+            double doubleValue
+            #endregion
+        }
+    }
+}
