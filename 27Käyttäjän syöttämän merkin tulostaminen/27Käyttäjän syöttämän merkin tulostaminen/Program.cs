84b2036ee017a411b63db0bfa02e3575f8b5afd0
Miksu21 <mikaahonen21@gmail.com>
2023-04-04 19:02:01 +0300

    4.4/2

---
+++ 27Käyttäjän syöttämän merkin tulostaminen/27Käyttäjän syöttämän merkin tulostaminen/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _27Käyttäjän_syöttämän_merkin_tulostaminen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {// Käyttäjän syöttämä merkki
+            Console.Write("Syötä numero: ");
+            int number = int.Parse(Console.ReadLine());
+
+            Console.Write("Syötä merkki: ");
+            char cMerkki = char.Parse( Console.ReadLine());
+
+            int i = 1;
+            for(i = 1; i<= number; i++)
+            {
+              Console.WriteLine(cMerkki);
+
+            }
+            Console.WriteLine();
+
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ 30c Kertymä/30c Kertymä/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _30c_Kertymä
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//30C C# Toisto: Kertymä (Yhdessä päivä 5)
+            Console.Write("Syötä kierroslukumäärä: ");
+            int loops = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            for (int i = 0; i < loops; i++)
+            {
+                sum = sum + i;
+
+            }
+            Console.WriteLine($"{loops} kierrosta kertymäksi saatiin: {sum}");
+
+
+               
+        }
+    }
+}
