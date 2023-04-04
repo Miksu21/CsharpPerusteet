f6d4c5b0232aca7e1c6a9a7e8f35a402a0213a7d
Miksu21 <mikaahonen21@gmail.com>
2023-04-04 17:35:34 +0300

    Tehtäviä

---
+++ Opetuspäivä 5/Opetuspäivä 5/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opetuspäivä_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//Toisto: Parillinen Pariton 28 c#
+
+            Console.Write("Syötä luku johon asti tulostetaan: ");
+            int number = int.Parse(Console.ReadLine());
+
+            int i = 1;
+
+            Console.Write("Parillinen / Pariton: ");
+            string input = Console.ReadLine();
+
+
+            while (i <= number)
+            {
+                if (i % 2 != 0 && input == "Pariton")
+
+                {
+                    Console.WriteLine(i);
+                }
+                i++;
+
+
+            }
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
